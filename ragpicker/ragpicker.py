ec44a4fbcb98672b2f3ee9e3ded93e4414a338ee
Jeremy Wagner-Kaiser <kalium@gmail.com>
2016-09-06 15:10:11 -0700

    Trim down ragpicker's config. No processing or similar. Only Viper. Leaver crawlers in place.

--- ragpicker/ragpicker.py
+++ ragpicker/ragpicker.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
-#                              _        _                
-#   _ __   __ _   __ _  _ __  (_)  ___ | | __  ___  _ __ 
+#                              _        _
+#   _ __   __ _   __ _  _ __  (_)  ___ | | __  ___  _ __
 #  | '__| / _` | / _` || '_ \ | | / __|| |/ / / _ \| '__|
-#  | |   | (_| || (_| || |_) || || (__ |   < |  __/| |   
-#  |_|    \__,_| \__, || .__/ |_| \___||_|\_\ \___||_|   
-#                |___/ |_|                               
+#  | |   | (_| || (_| || |_) || || (__ |   < |  __/| |
+#  |_|    \__,_| \__, || .__/ |_| \___||_|\_\ \___||_|
+#                |___/ |_|
 #
-# Plugin based malware crawler. 
-# Use this tool if you are testing antivirus products, collecting malware 
+# Plugin based malware crawler.
+# Use this tool if you are testing antivirus products, collecting malware
 # for another analyzer/zoo.
 # Many thanks to the cuckoo-sandbox team for the Architectural design ideas.
-# Includes code from cuckoo-sandbox (c) 2013 http://www.cuckoosandbox.org 
+# Includes code from cuckoo-sandbox (c) 2013 http://www.cuckoosandbox.org
 # and mwcrawler, (c) 2012 Ricardo Dias
 #
 # http://code.google.com/p/malware-crawler/
@@ -49,7 +49,7 @@ from core.constants import RAGPICKER_BUILD_DATE
 from core.constants import RAGPICKER_ROOT
 from core.constants import RAGPICKER_VERSION
 from core.worker import Worker
-from utils.logo import logo 
+from utils.logo import logo
 
 
 try:
@@ -64,7 +64,7 @@ log = logging.getLogger("Main")
 
 def main():
 	mapURL = {}
-	
+
 	logo()
 	parser = argparse.ArgumentParser(description='Ragpicker Malware Crawler')
 	parser.add_argument("-a", "--artwork", help="Show artwork", action="store_true", required=False)
@@ -77,9 +77,9 @@ def main():
 	parser.add_argument('--log-filename', help='logging filename')
 	parser.add_argument('--version', action='version', version='Ragpicker version ' + RAGPICKER_VERSION)
 
-	global args 
+	global args
 	args = parser.parse_args()
-	
+
 	if args.artwork:
 		try:
 			while True:
@@ -87,7 +87,7 @@ def main():
 				logo()
 		except KeyboardInterrupt:
 			return
-		
+
 	if args.log_level:
 		log_conf = dict(level=args.log_level,
 			format='%(levelname)s %(name)s %(module)s:%(lineno)d %(message)s')
@@ -97,16 +97,16 @@ def main():
 			log.info("log-filename: " + args.log_filename)
 
 		logging.basicConfig(**log_conf)
-	
+
 	if args.delete:
 		worker = Worker()
 		worker.runDelete()
 		return
-	
+
 	if args.info:
 		printRagpickerInfos(True)
 		return
-	
+
 	if args.url:
 		log.info(color("Download and analysis from %s" % args.url, RED))
 		runWorker(args.url)
@@ -114,121 +114,121 @@ def main():
 		printRagpickerInfos()
 		log.info(color("Load files from local directory %s" % args.directory, RED))
 		mapURL = getLocalFiles(args.directory)
-	else:			
+	else:
 		printRagpickerInfos()
-		# Malware URLs Crawlen 
+		# Malware URLs Crawlen
 		mapURL = runCrawler()
-		
+
 	# Max Threads=6
 	if args.processes > 6:
 		args.processes = 6
-		
-	log.info(color("Processes: " + str(args.processes), RED))	
+
+	log.info(color("Processes: " + str(args.processes), RED))
 	log.info(color("Process " + str(len(mapURL)) + " URLs", RED))
-	
+
 	# Create Process Pool
 	pool = Pool(processes=args.processes)
-	
+
 	# Malware Download, process and reporting
 	for url in mapURL.values():
 		pool.apply_async(runWorker, args=(url,))
-		
+
 	pool.close()
 	pool.join()
-		
+
 def getLocalFiles(directory):
 	mapURL = {}
-	
+
 	# give the queue some data
 	for root, dirs, files in walk(directory):
 		for file in files:
-			# do concatenation here to get full path 
+			# do concatenation here to get full path
 			full_path = join(root, file)
 			md5 = hashlib.md5(full_path).hexdigest()
 			mapURL[md5] = "file://" + full_path
-			
+
 	return mapURL
-			
+
 def runCrawler():
 	mapURL = {}
-	cfgCrawler = Config(os.path.join(RAGPICKER_ROOT, 'config', 'crawler.conf'))	
-	
+	cfgCrawler = Config(os.path.join(RAGPICKER_ROOT, 'config', 'crawler.conf'))
+
 	# Build the PluginManager
 	crawlerPluginManager = PluginManager()
 	crawlerPluginManager.setPluginPlaces(["crawler"])
 	crawlerPluginManager.collectPlugins()
-	
+
 	# Trigger run from the "Crawler" plugins
 	for pluginInfo in sorted(crawlerPluginManager.getAllPlugins(), key=lambda PluginInfo: PluginInfo.name):
 		crawlerModul = pluginInfo.plugin_object
-		
+
 		# Config for crawler module
 		try:
 			options = cfgCrawler.get(pluginInfo.name)
 			crawlerModul.set_options(options)
 		except Exception:
-			log.error("Crawler module %s not found in configuration file", pluginInfo.name)  
-			
+			log.error("Crawler module %s not found in configuration file", pluginInfo.name)
+
 		# If the crawler module is disabled in the config, skip it.
 		if not options.enabled:
 			continue
-		
+
 		try:
 			log.debug("Run Crawler: " + pluginInfo.name)
 			returnMap = crawlerModul.run()
 			mapURL.update(returnMap)
 		except Exception as e:
 			log.error('Error (%s) in %s', e, pluginInfo.name)
-	
+
 	return mapURL
 
 def runWorker(url):
 	log.info("Worker URL: " + url)
 	worker = Worker()
 	worker.run(url)
-		
+
 def printRagpickerInfos(print_options=False):
 	infolog = logging.getLogger("Info")
 	infolog.info(color("RAGPICKER_VERSION: " + RAGPICKER_VERSION, RED))
 	infolog.info(color("RAGPICKER_BUILD_DATE: " + RAGPICKER_BUILD_DATE, RED))
 	infolog.info(color("RAGPICKER_ROOT: " + RAGPICKER_ROOT, RED))
 	infolog.info("")
-	
+
 	pluginPlaces = ["crawler", "preProcessing", "processing", "reporting"]
-	
+
 	for place in pluginPlaces:
 		infolog.info(color("%s| " % (place + " ").upper().ljust(14, '-'), MAGENTA))
 		cfg = Config(os.path.join(RAGPICKER_ROOT, 'config', place + '.conf'))
 		ragpickerPluginManager = PluginManager()
 		ragpickerPluginManager.setPluginPlaces([place])
 		ragpickerPluginManager.collectPlugins()
-		
+
 		for pluginInfo in sorted(ragpickerPluginManager.getAllPlugins(), key=lambda PluginInfo: PluginInfo.name):
-			options = cfg.get(pluginInfo.name) 
-			
+			options = cfg.get(pluginInfo.name)
+
 			if options.enabled:
-				infolog.info(color("%s V%s   %s", MAGENTA) % ("              |----[+] " + 
+				infolog.info(color("%s V%s   %s", MAGENTA) % ("              |----[+] " +
 						pluginInfo.name.ljust(25), pluginInfo.version, pluginInfo.description))
 				if print_options:
 					for key, value in options.iteritems():
 						if key != "enabled":
 							infolog.info(color("                    |-- %s = %s", MAGENTA) % (key, str(value)))
 			else:
-				infolog.info(color("%s V%s   %s", BLUE) % ("              |----[-] " + 
+				infolog.info(color("%s V%s   %s", BLUE) % ("              |----[-] " +
 						pluginInfo.name.ljust(25), pluginInfo.version, pluginInfo.description))
-	
+
 	infolog.info("")
 	infolog.info("            %s %s" % (color("([+] = enabled)", MAGENTA), color("([-] = disabled)", BLUE)))
 	infolog.info("")
-	
-	checkVersion()
-				
+
+	# checkVersion()
+
 	sys.stdout.flush()
 
 def checkVersion():
 	infolog = logging.getLogger("Info")
 	infolog.info(color("Checking for Ragpicker updates...", RED))
-	
+
 	try:
 		request = urllib2.Request(VERSION_URL)
 		response = urllib2.urlopen(request, timeout=60)
@@ -244,7 +244,7 @@ def checkVersion():
 
 	server_version = getServerVersion(response_data)
 	current_version = getCurrentVersion()
-	
+
 	if server_version["major"] == current_version["major"] and server_version["minor"] == current_version["minor"] and server_version["build"] == current_version["build"]:
 		infolog.info(color("You have the latest version", GREEN))
 	else:
