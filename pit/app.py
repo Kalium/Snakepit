1efef5fa03751eeb249c4edca40cedea7630f33d
Jeremy Wagner-Kaiser <kalium@gmail.com>
2016-09-08 11:47:06 -0700

    Service now fully working. Exposes all endpoints. Exposes Swagger JSON. There's a Swagger UI container.

--- pit/app.py
+++ pit/app.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
-# from flask_restplus import Api, Resource, fields
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
+import flask_restless_swagger
+from flask_restless_swagger import SwagAPIManager as APIManager
 from sqlalchemy.dialects.postgresql import JSON
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/swagger.json": {"origins": "*"}})
 default_db = "postgresql://pit:pit@localhost:5432/pit"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PIT_DB', default_db)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,24 +16,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+flask_restless_swagger.sqlalchemy_swagger_type['JSON'] = "string"
+manager = APIManager(app, flask_sqlalchemy_db=db)
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hash = db.Column(db.String(64))
     parent_id = db.Column(db.Integer, db.ForeignKey("item.id"))
-    parent = db.relationship("parent")
-    children = db.relationship("Item", back_populates="parent")
-    data = db.relationship("Analysis", back_populates="item")
+    parent = db.relationship("Item", backref="children", remote_side=[id])
+    data = db.relationship("Analysis")
 
 
 class Analysis(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("item.id"))
-    item = db.relationship("item")
+    item = db.relationship("Item")
     data = db.Column(JSON)
 
+manager.create_api(Item, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Analysis, methods=['GET', 'POST', 'DELETE'])
+
 if __name__ == '__main__':
-    manager.run()
+    db.create_all()
+    app.run()
